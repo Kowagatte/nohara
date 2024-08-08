67e5390fbe297f4491a2d9e9c32192f5466e240e
Kowagatte <nnryanp@gmail.com>
2024-08-08 01:43:25 -0600

    Optimized radial generation a bit, no longer calculate darkness for pixels not within any points bounds

--- assets/RadialGradientGenerator/GradientGenerator.gd
+++ assets/RadialGradientGenerator/GradientGenerator.gd
@@ -13,12 +13,17 @@ func _init(_size: Vector2, _seed: int):
 	#blur()
 
 func generate_points():
-	var points = []
-	var min = (_size.x/256)
-	points.resize(rng.randi_range(min, min+3))
-	for p in range(points.size()):
-		points[p] = Vector2(rng.randi_range(_size.x/5, (_size.x-1)-(_size.x/5)), rng.randi_range(_size.y/5, (_size.y-1)-(_size.y/5)))
-	return points
+	var poisson = Poisson.new(rng.randi(), true, 384, _size)
+	#print(poisson.data)
+	#poisson.getImage().save_png("C:\\Users\\craft\\Documents\\poisson.png")
+	return poisson.data
+	
+	#var points = []
+	#var min = (_size.x/256)
+	#points.resize(rng.randi_range(min, min+1))
+	#for p in range(points.size()):
+		#points[p] = Vector2(rng.randi_range(_size.x/5, (_size.x-1)-(_size.x/5)), rng.randi_range(_size.y/5, (_size.y-1)-(_size.y/5)))
+	#return points
 
 func calculateMinRadius(point: Vector2):
 	var distances = []
@@ -37,34 +42,39 @@ func calculateMinRadius(point: Vector2):
 
 func generate():
 	
+	var start = Time.get_ticks_usec()
 	var points2d = generate_points()
+	var end = Time.get_ticks_usec()
+	print("generate points: "+str((end-start)/1000000.0)+"s")
+	
+	
 	var points = []
+	start = Time.get_ticks_usec()
 	for point in points2d:
 		var radius = calculateMinRadius(point)
 		points.append(Vector3(point.x, point.y, radius))
-		
-	#var image = Image.create(_size.x, _size.y, false, Image.FORMAT_RGBA8)
-	#image.fill(Color(255, 255, 255))
+	end = Time.get_ticks_usec()
+	print("Calculate radius': "+str((end-start)/1000000.0)+"s")
 	
-	#var ratio = _size.x / _size.y
-	
+	start = Time.get_ticks_usec()
 	data = []
 	for i in range(_size.x):
 		data.append([])
 		for j in range(_size.y):
 			data[i].append(1.0)
+	end = Time.get_ticks_usec()
+	print("Array instantialization': "+str((end-start)/1000000.0)+"s")
 	
+	start = Time.get_ticks_usec()
 	for x in range(_size.x):
 		for y in range(_size.y):
-			
 			for p in points:
 				var dist = Vector2(x, y).distance_to(Vector2(p.x, p.y))
-				var color = _gradient.sample(dist / p.z)
-				data[x][y] = data[x][y] * (1.0 - (1.0 - color.r))
-				#pixels[x][y] = pixels[x][y].darkened(1 - color.r)
-			
-			#image.set_pixel(x, y, pixels[x][y])
-	
+				if dist <= p.z:
+					var darkness = _gradient.sample(dist / p.z).r
+					data[x][y] = data[x][y] * (1.0 - ( 1.0 - (darkness)))
+	end = Time.get_ticks_usec()
+	print("darkness calculation': "+str((end-start)/1000000.0)+"s")
 
 func blur():
 	# loop through all pixels in image
@@ -85,3 +95,11 @@ func blur():
 							pass
 				average = average / count
 				data[i][j] = average
+
+func createImage():
+	var img = Image.create(_size.x, _size.y, false, Image.FORMAT_RGB8)
+	for x in range(_size.x):
+		for y in range(_size.y):
+			var c = data[x][y]
+			img.set_pixel(x, y, Color(c, c, c))
+	img.save_png("C:\\Users\\craft\\Documents\\test.png")
--- assets/RadialGradientGenerator/test.gd
+++ assets/RadialGradientGenerator/test.gd
@@ -5,14 +5,10 @@ func _ready():
 	var start = Time.get_ticks_usec()
 	var gradient = GradientGenerator.new(Vector2(1024, 1024), 1337)
 	var end = Time.get_ticks_usec()
-	print(str((end-start)/1000000.0)+"s")
+	print("Total time: "+str((end-start)/1000000.0)+"s")
 	
-	#var img = Image.create(1024, 1024, false, Image.FORMAT_RGB8)
-	#for x in range(gradient._size.x):
-		#for y in range(gradient._size.y):
-			#var c = gradient.data[x][y]
-			#img.set_pixel(x, y, Color(c, c, c))
-	#img.save_png("C:\\Users\\craft\\Documents\\test.png")
+	gradient.createImage()
+	
 	
 	pass # Replace with function body.
 
